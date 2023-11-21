f038160c5dc9f0591c0e5cb727dd147bcf99c533
Olena Yastrebtsova <uaalena202@gmail.com>
2023-11-21 10:22:32 +0200

    new commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+﻿
+document.addEventListener("DOMContentLoaded", function() {
+    var nav = document.getElementById('nav');
+    var closeButton = document.getElementById('close');
+    var menuButton = document.getElementById('menu');
+
+    closeButton.addEventListener('click', function() {
+        nav.style.top = '-300%';
+    });
+
+    menuButton.addEventListener('click', function() {
+        nav.style.top = '0';
+    });
+});
+
